ea15c754ecc08c0d6239d18818e223c3fc656c64
Aryan Sarvaiya <sarvaiyaaryan675@gmail.com>
2026-04-02 14:09:22 +0530

    connect frontend to backand

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState("Loading...");
 
   useEffect(() => {
-    fetch("https://devops-production-project.onrender.com/")
-      .then(res => res.text())
-      .then(data => setMessage(data));
+    fetch("https://devops-production-project.onrender.com/api/status")
+      .then((res) => res.json())
+      .then((data) => setStatus(data.status))
+      .catch(() => setStatus("Error connecting to backend"));
   }, []);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 DevOps Full Stack Project</h1>
-      <p>{message}</p>
+      <p>{status}</p>
     </div>
   );
 }
