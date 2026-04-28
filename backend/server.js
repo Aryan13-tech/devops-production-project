44381e1b379d005a014584bd6de185f34bc91d2a
Aryan Sarvaiya <sarvaiyaaryan675@gmail.com>
2026-04-28 13:37:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("this is the updated message from me!");
